4830c672a02bc58fc8e5bc832c029f449c5f8300
Chris <8133496+chriswmann@users.noreply.github.com>
2024-04-10 17:20:44 +0100

    Rename Progress struct to ProgressBar so docs are clearer

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,25 @@
-#![deny(missing_docs)]
-#![deny(clippy::missing_docs_in_private_items)]
-#![deny(rustdoc::broken_intra_doc_links)]
+#![deny(
+    missing_docs,
+    clippy::missing_docs_in_private_items,
+    clippy::missing_errors_doc,
+    clippy::missing_panics_doc
+)]
 //! A simple progress bar for iterators
 
 const CLEAR: &str = "\x1B[2J\x1B[1;1H";
 
+/// A wrapper around an iterator that prints a progress bar
 #[derive(Debug)]
-struct Progress<Iter> {
+pub struct ProgressBar<Iter> {
     iter: Iter,
     i: usize,
     bound: Option<usize>,
 }
 
-impl<Iter> Progress<Iter> {
+/// Create a new `Progress` struct from an iterator
+impl<Iter> ProgressBar<Iter> {
+
+    /// Create a new 
     pub fn new(iter: Iter) -> Self {
         Self {
             iter,
@@ -22,7 +29,7 @@ impl<Iter> Progress<Iter> {
     }
 }
 
-impl<Iter> Progress<Iter>
+impl<Iter> ProgressBar<Iter>
 {
     fn format_bar(&self) -> String {
         match self.bound {
@@ -32,7 +39,7 @@ impl<Iter> Progress<Iter>
     }
 }
 
-impl<Iter> Progress<Iter>
+impl<Iter> ProgressBar<Iter>
 where
     Iter: ExactSizeIterator,
 {
@@ -42,7 +49,7 @@ where
     }
 }
 
-impl<Iter> Iterator for Progress<Iter>
+impl<Iter> Iterator for ProgressBar<Iter>
 where
     Iter: Iterator,
 {
@@ -58,16 +65,32 @@ where
     
 }
 
-trait ProgressIteratorExt: Sized {
-    fn progress(self) -> Progress<Self>;
+trait ProgressBarIteratorExt: Sized {
+    fn progress(self) -> ProgressBar<Self>;
 }
 
-impl<Iter> ProgressIteratorExt for Iter
+impl<Iter> ProgressBarIteratorExt for Iter
 where
     Iter: Iterator,
 {
-    fn progress(self) -> Progress<Self> {
-        Progress::new(self)
+    fn progress(self) -> ProgressBar<Self> {
+        ProgressBar::new(self)
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_progress_still_iterates() {
+        let v = vec![1, 2, 3, 4, 5];
+        let mut p = v.iter().progress().with_bound();
+        assert_eq!(p.next(), Some(&1));
+        assert_eq!(p.next(), Some(&2));
+        assert_eq!(p.next(), Some(&3));
+        assert_eq!(p.next(), Some(&4));
+        assert_eq!(p.next(), Some(&5));
+        assert_eq!(p.next(), None);
+    }
+}
